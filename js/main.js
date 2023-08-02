33ddc10b66b428123e39f89bd6b5698d8f270fbc
Oscar Roman <oscarroman.pe@gmail.com>
2023-08-01 19:45:38 -0500

    add submenu for languages

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
+/* LANGUAGE SELECTOR */
+var dropdown = document.querySelector('.language-dropdown');
+
+// Evento click para mostrar y ocultar
+dropdown.addEventListener('click', function(event) {
+    event.stopPropagation();
+    dropdown.querySelector('.language-menu').classList.toggle('show');
+});
+  
+// Ocultar menú desplegable al hacer clic fuera
+window.onclick = function(event) {
+    if (!event.target.matches('.dropdown-toggle')) {
+        var dropdowns = document.querySelectorAll('.navbar__menu-dropdown');
+        dropdowns.forEach(function(dropdown) {
+            dropdown.classList.remove('show');
+        });
+    }
+};
+
+/* MOBILE MENU */
+
 const nav = document.querySelector("#navbar");
 const openNav = document.querySelector("#open");
 const closeNav = document.querySelector("#close");
@@ -5,28 +26,31 @@ const menuItems = document.querySelectorAll('.navbar__menu__item');
 
 openNav.addEventListener("click", () => {
     nav.classList.add("visible");
-})
+});
 
 closeNav.addEventListener("click", () => {
     nav.classList.remove("visible");
-})
+});
 
-// document.addEventListener('click', function(event) {
-//     if(!event.target.closest('#navbar') && !event.target.matches('#navbar')) {
-//         nav.classList.remove("visible");
-//     }
-// });
+menuItems.forEach(function(menuItem) {
+    menuItem.addEventListener('click', function(event) {
+        let targetElement = event.target; // clicked element
 
+        do {
+            if (targetElement == menuItem && targetElement.parentElement.classList.contains('language-dropdown')) {
+                // This is a click inside the language dropdown, so just return and do nothing.
+                return;
+            }
+            // Go up the DOM.
+            targetElement = targetElement.parentNode;
+        } while (targetElement);
 
-
-menuItems.forEach(function(menuItems) {
-    menuItems.addEventListener('click', function() {
+        // This is a click outside the language dropdown, so close the menu.
         nav.classList.remove("visible");
     });
 });
 
 //This is for disappearing when a click is done outside the menu
-
 document.addEventListener('click', function(event) {
     if (!nav.contains(event.target) && !openNav.contains(event.target)) {
         nav.classList.remove("visible");
