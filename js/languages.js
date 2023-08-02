0953789c8dc46f87760af011f3a1d7c5eb1191b6
Oscar Roman <oscarroman.pe@gmail.com>
2023-08-02 06:53:53 -0500

    Language en, es added. Updated projects

---
+++ js/languages.js
@@ -0,0 +1,14 @@
+let lang = navigator.language || navigator.userLanguage;
+lang = lang.substr(0, 2);
+
+switch (lang) {
+    case 'en':
+        window.location.href = '../index-en.html';
+        break;
+    case 'es':
+        window.location.href = '../index-es.html';
+        break;
+    default:
+        window.location.href = '/index-en.html';
+        break;
+}
