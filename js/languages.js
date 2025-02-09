66f50237ef8d2b6e529e2e300f338d1a57cc0aef
Oscar Roman Quispe <oscarroman.pe@gmail.com>
2025-02-08 23:23:06 -0500

    image path fixes

--- js/languages.js
+++ js/languages.js
@@ -1,16 +1,16 @@
-'use strict';
+"use strict";
 
 let lang = navigator.language || navigator.userLanguage;
 lang = lang.substr(0, 2);
 
 switch (lang) {
-    case 'en':
-        window.location.href = '../index-en.html';
+    case "en":
+        window.location.href = "./index-en.html";
         break;
-    case 'es':
-        window.location.href = '../index-es.html';
+    case "es":
+        window.location.href = "./index-es.html";
         break;
     default:
-        window.location.href = '/index-en.html';
+        window.location.href = "./index-en.html";
         break;
 }
